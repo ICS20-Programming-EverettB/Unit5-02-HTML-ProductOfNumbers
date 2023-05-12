e8181a9be14d3770e88484b055e0e2af510ca5c6
EverettBernard <124380095+EverettBernard@users.noreply.github.com>
2023-05-12 14:43:15 +0000

    finnished

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 
 "use strict";
 /**
- * This function dislays numbers between the users min and max .
+ * This function multiples 2 numbers using for loops.
  */
 function calculate () {
   
@@ -12,21 +12,38 @@ function calculate () {
   let num1 = parseInt(document.getElementById('num1').value);
   let num2 = parseInt(document.getElementById('num2').value);
   
+  // absolute number 1 and 2 variables  
+  let absnum1 = Math.abs(num1);
+  let absnum2 = Math.abs(num2);
+  
   //declaring product = 0
   let product= 0
   
-  // number open string
-  for (let counter = 1; counter <= num2; counter++) {
+  // for loop to multiply absnum1 and absnum2
+  for (let counter = 1; counter <= absnum2; counter++) {
     
-    product =  product + num1
+    product =  product + absnum1
 
   }
   
-  //display numbers
-  let numbers = num1 + "x" + num2 + "=" + product
+  // if statments to multiply a negative x a negative number
+  // if statment for product times negative 1 if number is less than 0 making it a positive 
+  if ((num1 < 0) && (num2>0)) {
+  product = product*-1
+}
+    
+  // else if for product times negative 1 if number is less than 0 making it a positive 
+  else if ((num2 < 0) && (num1>0)) {
+   product = product*-1
+}
   
-  // output all intigers between min and max
+ //declare numbers to include product and formula 
+  let numbers = num1 + " x " + num2 + "= " + product
+  
+  // output numbers to user
   document.getElementById('display-multiply').innerHTML = numbers
 
 }
+ 
+
  
